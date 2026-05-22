2867ae5e613ebe4cba6b860714e0ea972aecdd8f
Aditya <aditya.gupta@aaravtech.net>
2026-05-22 22:16:32 +0530

    chore: update dependencies

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,7 +11,7 @@
 	$(document).ready(function() {
 
 		/* ==================================================
-		    # Tooltip Init
+		# Tooltip Init
 		===============================================*/
 		$('[data-toggle="tooltip"]').tooltip();
 
