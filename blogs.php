f45a5b9b2ba747a66278da49477d5decc55cce4d
Aditya <aditya.gupta@aaravtech.net>
2026-05-28 04:48:51 +0530

    chore: update dependencies

--- blogs.php
+++ blogs.php
@@ -30,6 +30,22 @@ if ($result === false) {
 <html lang="en">
 
 <head>
+    <script>
+        (function(w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push({
+                'gtm.start': new Date().getTime(),
+                event: 'gtm.js'
+            });
+            var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s),
+                dl = l != 'dataLayer' ? '&l=' + l : '';
+            j.async = true;
+            j.src =
+                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+            f.parentNode.insertBefore(j, f);
+        })(window, document, 'script', 'dataLayer', 'GTM-NTDBTHX9');
+    </script>
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-1PMD21XQXM"></script>
 
     <!-- ========== Meta Tags ========== -->
@@ -218,7 +234,8 @@ if ($result === false) {
 </script>
 
 <body>
-
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NTDBTHX9"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
     <!-- Header 
     ============================================= -->
     <header>
